a0ca9cd73357aac9e11e757cc8f07c9572dbfbc6
KapyshonTvBro6 <dany2008email@mail.ru>
2026-02-05 14:21:31 +0300

    Link project

--- Source/Marine_Monopolist/Marine_Monopolist.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Marine_Monopolist.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Marine_Monopolist, "Marine_Monopolist" );
-
-DEFINE_LOG_CATEGORY(LogMarine_Monopolist)
- 
--- Source/Marine_Monopolist/Marine_MonopolistGameMode.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Marine_MonopolistGameMode.h"
-
-AMarine_MonopolistGameMode::AMarine_MonopolistGameMode()
-{
-	// stub
-}
